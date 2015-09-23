5c7d635c6d8b047018ca1cb83a1c1022db60d7d0
softdevstory <developer@softdevstory.net>
2015-09-23 22:09:00 +0900

    web-mode configuration is modified.

--- lisp/my-web.el
+++ lisp/my-web.el
@@ -1,11 +1,26 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+(add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
+
+  (setq web-mode-style-padding 2)
+  (setq web-mode-script-padding 2)
+
+;  (setq web-mode-block-padding 0)
+;  (setq web-mode-comment-style 2)
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
