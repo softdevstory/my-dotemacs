364c359db3242a74fbd3696b0e3bae466bc0ddbd
softdevstory <developer@softdevstory.net>
2017-08-29 18:44:08 +0900

    set coding font for each OS

--- lisp/my-theme.el
+++ lisp/my-theme.el
@@ -6,6 +6,12 @@
 
 ;;(require 'sublime-themes)
 ;;(load-theme 'spolsky t)
-(set-frame-font "Menlo")
-;;(custom-set-faces '(default ((t (:family "Menlo")))))
-;;(custom-set-faces '(default ((t (:family "D2Coding")))))
+
+(cond
+ ((find-font (font-spec :name "Menlo")) ;; for mac OS
+  (set-frame-font "Menlo"))
+  ((find-font (font-spec :name "Consolas")) ;; for Windows
+   (set-frame-font "Consolas"))
+  ((find-font (font-spec :name "DejaVu Sans Mono")) ;; for Linux
+   (set-frame-font "DejaVu Sans Mono")))
+
